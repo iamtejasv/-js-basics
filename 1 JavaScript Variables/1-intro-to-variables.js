1524ae2361b3a38267d41e382c75ed698d0113ec
iamtejasv <tejasv0804@gmail.com>
2024-08-16 21:04:36 +0530

    Add notes for section 1

---
+++ 1 JavaScript Variables/1-intro-to-variables.js
@@ -0,0 +1,15 @@
+// The var keyword is the previous keyword used to create a variable.
+// Declaring and assigning a variable on the same line is known as initialization;
+var previousNamingConvention = 'Tejas';
+
+// the let keyword is the modern syntax to create a variable
+let firstName = 'Tejas';
+
+// Variable declaration
+let variableDeclaration;
+
+// Variable assignment. The equal sign, =, is known as the assignment operator.
+variableDeclaration = 'value';
+
+// You output to the console with console.log();
+console.log(variableDeclaration);
