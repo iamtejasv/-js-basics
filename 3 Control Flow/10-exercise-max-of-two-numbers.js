ee97b6ee27aad392a0b4548e03ff493576bf1ec3
iamtejasv <tejasv0804@gmail.com>
2024-09-05 12:30:40 +0530

    Add exercise max of two numbers in section 3

---
+++ 3 Control Flow/10-exercise-max-of-two-numbers.js
@@ -0,0 +1,7 @@
+// Implement a function that accepts two numbers and returns the maximum number.
+
+function maxNum(num1, num2) {
+    return num1 >= num2 ? num1 : num2;
+}
+
+console.log( maxNum(2, 1) );
